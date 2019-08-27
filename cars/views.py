c05c6580e42033ee911df94541fca13cf2e2357b
samirahisham <engineersamiraawad@gmail.com>
2019-08-27 09:57:33 +0300

    almost done

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Car
+from .forms import CarsForm
+
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -19,14 +21,35 @@ def car_detail(request, car_id):
 
 def car_create(request):
 	#Complete Me
-	return render(...)
+	form=CarsForm()
+	if request.method =="POST":
+		form=CarsForm(request.POST,request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect("car-list")
+
+	context={
+		"form":form,
+
+	}
+	return render(request,'car_create.html',context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car= Car.objects.get(id=car_id)
+	form =CarsForm(instance=car)
+	if request.method=="POST":
+		form=CarsForm(request.POST,request.FILES, instance= car)
+		if form.is_valid():
+			form.save()
+			return redirect("car-list")
+	context={
+		"form":form,
+		"car":car,
+	}
+	return render(request,"car_update.html",context)
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	car= Car.objects.get(id=car_id).delete()
+	return redirect("car-list")
